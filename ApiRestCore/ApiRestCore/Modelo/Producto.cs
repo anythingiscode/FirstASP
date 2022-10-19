d4a2704ca52175ab8cd2cfdb9f7e430b0997f228
anythingiscode <rolkawa99@gmail.com>
2022-10-19 22:37:23 +0200

    Modelo y clase con Tabla "Producto"

---
+++ ApiRestCore/ApiRestCore/Modelo/Producto.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiRestCore.Modelo
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }    
+        public double PrecioCompra { get; set; }    
+        public double PrecioVenta { get; set; }
+        public int Stock { get; set; }
+    }
+}
